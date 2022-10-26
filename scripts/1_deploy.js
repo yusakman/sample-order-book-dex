e681c408f9f10571378bfdbb3f168beff28b148e
yusakman <yusakmanullang93@gmail.com>
2022-10-26 16:07:36 +0800

    Update test Token.js

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,20 +1,16 @@
-const { ethers } = require("hardhat");
-
 async function main() {
-  // Do stuff...
-
   // Fetch contract to deploy
-  const Token = await ethers.getContractFactory("Token");
-  const token = await Token.deploy()
+  const Token = await ethers.getContractFactory("Token")
 
   // Deploy contract
+  const token = await Token.deploy('Wonderful SBUCKS', 'WSB', '1000000')
   await token.deployed()
-  console.log(`Token deployed to: ${token.address}`)
+  console.log(`Token Deployed to: ${token.address}`)
 }
 
 main()
   .then(() => process.exit(0))
   .catch((error) => {
     console.error(error);
-    process.exitCode = 1;
+    process.exit(1);
   });
